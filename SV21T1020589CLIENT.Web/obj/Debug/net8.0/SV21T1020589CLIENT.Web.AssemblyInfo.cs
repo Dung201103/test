54552024ec83d06edb41b864ce1aff344802fce5
Dung201103 <21t1020322@husc.edu.vn>
2025-06-11 12:45:27 +0700

    Fix: include Lite.db in out appsetting

--- SV21T1020589CLIENT.Web/obj/Debug/net8.0/SV21T1020589CLIENT.Web.AssemblyInfo.cs
+++ SV21T1020589CLIENT.Web/obj/Debug/net8.0/SV21T1020589CLIENT.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SV21T1020589CLIENT.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ad1eed16066d68c9707825de529cbf1fd65eb7f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e54bb89c93e5d2a428f0adeaef5e79dfa2fa9e20")]
 [assembly: System.Reflection.AssemblyProductAttribute("SV21T1020589CLIENT.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SV21T1020589CLIENT.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
